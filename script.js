5eaa6cb48e18e353fa743386a9a61c7984a550b4
Amit <amit0805kumar@gmail.com>
2019-09-04 18:45:16 +0530

    initial files added

---
+++ script.js
@@ -0,0 +1,48 @@
+$(function () {
+
+
+
+    //Dom elements
+    var popupContainer = $('.popup__container');
+    var popup = $('.popup');
+    var popupBg = $('.popup__bg');
+    var close = $('.close');
+    var imgGallery = $('.imgGallery');
+
+    var imgSlider = $('.imgSlider');
+    var videoSlider = $('.videoSlider');
+
+    var videoGallery = $('.videoGallery');
+    var popup__image = $('.popup__image');
+
+    //SHowing Popup
+    imgGallery.click(function () {
+        videoSlider.addClass('hideSlider');
+        imgSlider.removeClass('hideSlider');
+        popup.toggleClass('hidePopup');
+        popupContainer.toggleClass('hidePopup');
+        // var title = $(this).attr("title");
+        console.log(this.children);
+    });
+
+    videoGallery.click(function () {
+        imgSlider.addClass('hideSlider');
+        videoSlider.removeClass('hideSlider');
+        popup.toggleClass('hidePopup');
+        popupContainer.toggleClass('hidePopup');
+        // var title = $(this).attr("title");
+    });
+
+    //Closing Poopup
+    close.click(function () {
+        popup.toggleClass('hidePopup');
+        popupContainer.toggleClass('hidePopup');
+    });
+    popupBg.click(function () {
+        popup.toggleClass('hidePopup');
+        popupContainer.toggleClass('hidePopup');
+    });
+
+    // console.log(imgBox[0].children);
+
+})
